f63fab29739713626e48276ffdd012dd433534d9
Jeff Austin <jeffaustin81@gmail.com>
2015-08-28 12:42:09 -0700

    Add initial routes and views

--- app/app.php
+++ app/app.php
@@ -25,7 +25,37 @@
     ));
 	
 	$app->get("/", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'form_check' => false));
+    });
+    
+    $app->get("/form_store", function() use ($app) {
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'form_check' => true));
+    });
+    
+    $app->post("/add_store", function() use ($app) {
+        $store = new Store($_POST['type']);
+        $store->save();
+
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'form_check' => false));
+    });
+    
+    $app->post("/delete_stores", function() use ($app) {
+        Store::deleteAll();
         
+        return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'form_check' => false));
+
+    });
+    
+    $app->get("/stores/{id}", function($id) use ($app) {
+        $store = Store::find($id);
+        
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => false));
+    });
+    
+    $app->get("/form_brand", function() use ($app) {
+        $store = Store::find($_GET['store_id']);
+        
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => true));
     });
 	
 	return $app;
