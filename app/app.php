1dbb064c5c63e01bd9b0ebf0e29d77d5ef94f470
Jeff Austin <jeffaustin81@gmail.com>
2015-08-28 14:26:33 -0700

    Add delete and update functionality to views

--- app/app.php
+++ app/app.php
@@ -39,6 +39,22 @@
         return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'form_check' => false));
     });
     
+    $app->get("/form_store_update", function() use ($app) {
+        $store = Store::find($_GET['store_id']);
+        
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'store_update' => true, 'form_check' => false));
+    });
+    
+    $app->patch("/update_store", function() use ($app) {
+        $name = $_POST['name'];
+        $store_id = $_POST['store_id'];
+
+        $store = Store::find($store_id);
+        $store->updateName($name);
+
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'store_update' => false, 'form_check' => false));
+    });
+        
     $app->post("/delete_stores", function() use ($app) {
         Store::deleteAll();
         
@@ -49,14 +65,39 @@
     $app->get("/stores/{id}", function($id) use ($app) {
         $store = Store::find($id);
         
-        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => false));
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => false, 'store_update' => false));
     });
     
     $app->get("/form_brand", function() use ($app) {
         $store = Store::find($_GET['store_id']);
         
-        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => true));
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => true, 'store_update' => false));
     });
+    
+    $app->post("/add_brand", function() use ($app) {
+        $name = $_POST['name'];
+        $brand = new Brand($name, $id = null);
+        $brand->save();
+        
+        $store_id = $_POST['store_id'];
+        $store = Store::find($store_id);
+        $store->addBrand($brand->getId());
+        
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => false, 'store_update' => false));
+    });
+    
+    $app->delete("/delete_brand/{id}", function($id) use ($app) {
+        
+        $id = $_POST['brand_id'];
+        $store_id = $_POST['store_id'];
+        
+        $store = Store::find($store_id);
+        $store->deleteBrand($id);
+
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'brands' => $store->getBrands(), 'form_check' => false, 'store_update' => false));
+    });
+    
+    
 	
 	return $app;
 	
